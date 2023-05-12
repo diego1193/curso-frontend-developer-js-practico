babb1a179638d1573e10e693c6d72b79e9afbec9
diego1193 <diegocabrera1193@gmail.com>
2023-05-12 12:26:47 -0500

    feat(Trabajando en proyecto real): integrando html, media query y js

--- main.js
+++ main.js
@@ -1,14 +1,16 @@
 const menuEmail = document.querySelector('.navbar-email')
 const desktoMenu = document.querySelector('.desktop-menu')
+const menuHamIcon = document.querySelector('.menu')
+const mobileMenu = document.querySelector('.mobile-menu')
 
 menuEmail.addEventListener("click", toggleDesktopMenu);
+menuHamIcon.addEventListener("click", toggleMobileMenu);
 
 function toggleDesktopMenu() {
     //desktoMenu.style.display = "block";
     desktoMenu.classList.toggle("inactive") // Quita y pone un elemento (de los estilos) q hayamos colocado en style.css
 
 }
-
 
 /*
 // Desparece si colocamos el mause afuera del div
@@ -19,3 +21,7 @@ desktoMenu.addEventListener("mouseover", ()=>{
 desktoMenu.addEventListener("mouseout", ()=>{
     desktoMenu.style.display = "none";
 })*/
+
+function toggleMobileMenu() {
+    mobileMenu.classList.toggle("inactive") // Quita y pone un elemento (de los estilos) q hayamos colocado en style.css
+}
